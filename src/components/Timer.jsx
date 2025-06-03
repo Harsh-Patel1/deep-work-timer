64c3c3b84f084c4b0474549968ad8fb7365bb071
Harsh Patel <hpatel1@iuw.edu>
2025-06-03 14:01:11 -0500

    Finished the timer formatting and count down logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Timer() {
-    const [timeleft, setTimeLeft] = useState(1500);
+    const [timeLeft, setTimeLeft] = useState(1500);
+    const mins = Math.floor(timeLeft/60).toString().padStart(2, '0');
+    const secs = (timeLeft % 60).toString().padStart(2, '0');
+    const formatTime = mins + ":" + secs;
+
+    useEffect (() => {
+        if (timeLeft == 0) return;
+
+        const interval = setInterval(()=> {
+            setTimeLeft(prev => prev - 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timeLeft])
 
     return (
         <div>
             <p>
-                {timeleft}
+                {formatTime}
             </p>
         </div>
     )
