999052e69cfa9bfd7c93ac12b80f48b1e15b35d0
Harsh Patel <hpatel1@iuw.edu>
2025-06-03 14:25:30 -0500

    Created and implemented the function of the  start, pause, and reset buttons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,25 +2,33 @@ import { useState, useEffect } from 'react';
 
 function Timer() {
     const [timeLeft, setTimeLeft] = useState(1500);
+    const [isRunning, setIsRunning] = useState(false); // Keeps track of if we are running the timer, T if running, F if paused
     const mins = Math.floor(timeLeft/60).toString().padStart(2, '0');
     const secs = (timeLeft % 60).toString().padStart(2, '0');
     const formatTime = mins + ":" + secs;
 
+
     useEffect (() => {
-        if (timeLeft == 0) return;
+        if (!isRunning || timeLeft == 0) return;
 
         const interval = setInterval(()=> {
             setTimeLeft(prev => prev - 1);
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [timeLeft])
+    }, [isRunning, timeLeft])
 
     return (
         <div>
             <p>
                 {formatTime}
             </p>
+            <button onClick={() => setIsRunning(true)}>Start</button>
+            <button onClick ={() => setIsRunning(false)}> Pause </button>
+            <button onClick = {()=> {
+                setIsRunning(false);
+                setTimeLeft(1500);
+            }}> Reset</button>
         </div>
     )
 }
