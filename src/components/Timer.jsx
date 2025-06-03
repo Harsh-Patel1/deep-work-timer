ad69332c647896ab77dc9b891a17b37a45c0ad92
Harsh Patel <hpatel1@iuw.edu>
2025-06-03 15:48:16 -0500

    Created a feature which allows the user to set a custom timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,15 +1,25 @@
 import { useState, useEffect } from 'react';
 
 function Timer() {
-    const [timeLeft, setTimeLeft] = useState(1500);
+    const [initialTime, setInitialTime] = useState(1500);
+    const [timeLeft, setTimeLeft] = useState(initialTime);
     const [isRunning, setIsRunning] = useState(false); // Keeps track of if we are running the timer, T if running, F if paused
+
+    // used for user input of custom time
+    const [inputMinutes, setInputMinutes] = useState();
+
     const mins = Math.floor(timeLeft/60).toString().padStart(2, '0');
     const secs = (timeLeft % 60).toString().padStart(2, '0');
+
     const formatTime = mins + ":" + secs;
 
 
+
+
+
+
     useEffect (() => {
-        if (!isRunning || timeLeft == 0) return;
+        if (!isRunning || timeLeft === 0) return;
 
         const interval = setInterval(()=> {
             setTimeLeft(prev => prev - 1);
@@ -23,11 +33,23 @@ function Timer() {
             <p>
                 {formatTime}
             </p>
+            <input
+            type="number"
+            placeholder="Enter minutes"
+            value={inputMinutes}
+            onChange={(e) => setInputMinutes(parseInt(e.target.value) || 0)}
+            />
+            <button onClick ={() => {
+                const totalSeconds = inputMinutes * 60;
+                setInitialTime(totalSeconds);
+                setTimeLeft(totalSeconds);
+            }}>Set Time</button>
+
             <button onClick={() => setIsRunning(true)}>Start</button>
             <button onClick ={() => setIsRunning(false)}> Pause </button>
             <button onClick = {()=> {
                 setIsRunning(false);
-                setTimeLeft(1500);
+                setTimeLeft(initialTime);
             }}> Reset</button>
         </div>
     )
